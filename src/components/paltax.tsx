68f3faaab3d322f90b2ed4ecd22edff205447a39
Adeline de Gaulejac <adelinedegaulejac@gmail.com>
2023-12-09 17:23:35 +0100

    added projects cards components and projects section desktop and mobile

---
+++ src/components/paltax.tsx
@@ -0,0 +1,43 @@
+import React, { FunctionComponent } from 'react';
+import { useTheme } from '../context/theme-context';
+import PaltaxPicture from '../assets/paltax-logo.png';
+
+const PaltaxProjectCard: FunctionComponent = () => {
+
+  const { theme } = useTheme();
+
+  return (
+    <div className="flex flex-row flex-wrap mx-auto">
+      {/* Card component */}
+      <div className="transition-all duration-150 flex w-full px-12 py-4 md:w-11/12">
+        <div className="flex flex-col items-stretch pt-6 mb-6 transition-all duration-150 bg-white rounded-3xl shadow-lg hover:shadow-2xl lg:mx-4">
+          {/* Card Title */}
+          <div className="w-full px-4 py-3 text-center font-title font-medium text-3xl text-jet md:text-3xl lg:5xl">
+            <h3>PALTAX</h3>
+          </div>
+          {/* Card image */}
+          <div className="md:flex-shrink-0 bg-[#FAFAFA]">
+            <img className="object-contain w-full md:h-56" src={PaltaxPicture} alt="Active Buddy's logo" />
+          </div>
+          {/* Card description */}
+          <div className="flex flex-row flex-wrap w-full h-26 px-4 py-4 text-base font-normal text-left text-jet lg:text-lg lg:h-28 lg:px-6">
+            <p>A web app to create, import and manage your recipes and to generate your shopping list.</p>
+          </div>
+          {/* Card tools */}
+          <div className="flex flex-row flex-wrap w-full h-14 px-4 py-2 text-base font-light text-left text-jet lg:px-6">
+            <p>Ruby on Rails and much more to come !</p>
+          </div>
+          {/* Card links */}
+          <div className="px-4 py-4 h-18 bg-jet rounded-b-[24px]">
+            {/* Work in progress message */}
+            <div className="flex flex-row h-16 justify-around items-center mx-auto px-12 py-2 bg-jet">
+              <p className="font-title text-floral">WORK IN PROGRESS</p>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+ );
+}
+
+export default PaltaxProjectCard;
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,16 +1,22 @@
 import React, { FunctionComponent } from 'react';
-import paltax from '../assets/paltax.png';
+import ActiveBuddyProjectCard from './activebuddy-project-card';
+import AlibyeProjectCard from './alibye-project-card';
+import PokedexProjectCard from './pokedex-project-card';
+import PaltaxProjectCard from './paltax';
 
 const Projects: FunctionComponent = () => {
 
   return (
     <section id="projects">
-      <div className="container mx-auto text-center md:text-left">
+      <div className="container mx-auto text-center md:text-left md:pl-7">
         <h2 className="font-title font-bold text-coral bg-gradient-to-b from-[#FF7D41] via-[#FF8B5C] to-[#FF9D85] inline-block text-transparent bg-clip-text text-4xl mb-8 md:text-5xl md:mb-4">PROJECTS</h2>
-      </div>      <img src={paltax} alt="Logo Ade Studio" />
-      <p>Lorem ipsum dolor sit amet consectetur adipiscing, elit tincidunt vehicula dapibus facilisi vestibulum, habitant in blandit etiam himenaeos. Phasellus per morbi mauris non pulvinar vestibulum varius class molestie, arcu torquent odio pharetra sollicitudin aliquam est aenean augue, in rhoncus integer aptent habitasse inceptos senectus himenaeos. Netus cursus feugiat nunc mus ut pretium laoreet, mattis leo sem curae vel vehicula, justo montes mollis hac viverra mi.
-
-      Ligula habitasse facilisis venenatis nascetur pharetra consequat eleifend quis lacinia, mattis tristique vestibulum urna curae conubia tortor class nulla vehicula, non leo augue montes cubilia justo est ornare. Hendrerit cum sagittis quisque magnis mi risus vehicula porttitor cursus, egestas libero iaculis auctor vivamus potenti leo aliquam, enim platea est commodo interdum tellus condimentum class. Etiam maecenas turpis fusce arcu ultrices massa pharetra felis at porta, platea ac fringilla commodo dictum posuere netus eu mattis luctus suspendisse, donec fames fermentum litora tristique tempor facilisi convallis rhoncus.</p>
+      </div>
+      <div className="md:grid xl:mx-32 md:grid-cols-2 md:gap-4 md:justify-between">
+        <ActiveBuddyProjectCard />
+        <AlibyeProjectCard />
+        <PokedexProjectCard />
+        <PaltaxProjectCard />
+      </div>
     </section>
   );
 }
